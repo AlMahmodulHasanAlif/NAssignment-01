ae85cd69d418d6974dc14820f1bfc5ab6076643c
unknown <almahmodulhasanalif@gmail.com>
2026-05-26 15:29:47 +0600

    problem 7 solved

--- solutions.ts
+++ solutions.ts
@@ -4,15 +4,19 @@ function filterEvenNumbers(Number:number[]):number[]{
     return Number.filter(num => num % 2 === 0);
 }
 
-const result = filterEvenNumbers([1, 2, 3, 4, 5, 6]); 
-console.log(result);
+// const result = filterEvenNumbers([1, 2, 3, 4, 5, 6]); 
+// console.log(result);
+
+
 
 //Problem-2
 function reverseString(String:string):string{
     return String.split('').reverse().join('');
 }
-const reversed = reverseString("is it working?");
-console.log(reversed);  
+// const reversed = reverseString("is it working?");
+// console.log(reversed);  
+
+
 
 //Problem-3
 type StringOrNumber = string | number;
@@ -24,8 +28,10 @@ function checkType(stringOrNumber: StringOrNumber): string|number {
         return ("Number");
     }
 }
-const type1 = checkType(42);
-console.log(type1);
+// const type1 = checkType(42);
+// console.log(type1);
+
+
 
 //Problem-4
 
@@ -33,9 +39,10 @@ function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
     return obj[key];
 }
 
-const user = { id: 1, name: "John Doe", age: 21 };
+// const user = { id: 1, name: "John Doe", age: 21 };
 
-console.log(getProperty(user, "name"));
+// console.log(getProperty(user, "name"));
+
 
 
 //Problem-5
@@ -49,9 +56,26 @@ function toggleReadStatus(book: Book): Book & { isRead: boolean } {
     return { ...book, isRead: true };
 }
 
-const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
+// const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
 
 
-console.log(toggleReadStatus(myBook));
+// console.log(toggleReadStatus(myBook));
+
+
+
 
 //Problem-6
+
+
+
+
+
+
+
+//Problem-7
+
+function getIntersection(one: number[], two: number[]): number[] {
+    return one.filter(value => two.includes(value));
+}
+
+console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6,7]));
